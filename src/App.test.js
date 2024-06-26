46856a9c3440a3203f14d20c7060bf61bac45cb4
APDevTest <160549292+APDevTest@users.noreply.github.com>
2024-06-26 15:39:26 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -11,6 +11,6 @@ it('should render', () => {
 
   expect(screen.getByText('Business Contacts')).toBeInTheDocument();
 });
-it('should do something amazing', () => {
-    expect(true).toBe(false);
-});
+// it('should do something amazing', () => {
+//     expect(true).toBe(false);
+// });
